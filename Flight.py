fa1b22ab8c05235d6fecaa5181202e13fcda0ff8
Nikittua <nisergeev_1@edu.hse.ru>
2023-03-05 20:58:25 +0300

    Добавил комментарии и убрал выполняющийся код

--- Flight.py
+++ Flight.py
@@ -1,36 +1,4 @@
-from datetime import datetime
-from typing import List
-import time
-from Flight_generator import *
-from algorithms import *
-import matplotlib.pyplot as plt
-import numpy as np
 
-
-def measure_sort_time(sort_function, data):
-    start = time.time()
-    sort_function(data)
-    end = time.time()
-    return end - start
-def plot_sort_time_vs_size(algorithms, flights):
-    size = np.arange(1, len(flights) + 1)
-    sort_times = []
-    labels = []
-    for algorithm in algorithms:
-        sort_time = np.zeros(len(flights))
-        for i in range(len(flights)):
-            sub_flights = flights[:i+1]
-            sort_time[i] = measure_sort_time(algorithm, sub_flights)
-        sort_times.append(sort_time)
-        labels.append(algorithm.__name__)
-
-    plt.figure(figsize=(8, 6))
-    for i in range(len(algorithms)):
-        plt.plot(size, sort_times[i], label=labels[i])
-    plt.legend()
-    plt.xlabel('Size of array')
-    plt.ylabel('Sorting time')
-    plt.savefig('sort_time_vs_size.png')
 
 class Flight:
     def __init__(self, flight_number, airline, arrival_date, arrival_time, passengers):
@@ -65,38 +33,3 @@ class Flight:
 
     def __ge__(self, other):
         return self.__eq__(other) or self.__gt__(other)
-
-
-
-def save_flights_to_file(flights, file_name):
-    with open(file_name, 'w') as f:
-        for flight in flights:
-            f.write(str(flight) + '\n')
-def load_flights_from_file(file_name):
-    flights = []
-    with open(file_name, 'r') as f:
-        for line in f:
-            flight_number, airline, arrival_date, arrival_time, passengers = line.strip().split(', ')
-            flights.append(Flight(flight_number, airline, arrival_date, arrival_time, int(passengers)))
-    return flights
-def measure_sort_times(data):
-    times = {}
-    times['quicksort_flights'] = measure_sort_time(quicksort_flights, data)*1000
-    times['bubble_sort_flights'] = measure_sort_time(bubble_sort_flights, data)*1000
-    times['shaker_sort_flights'] = measure_sort_time(shaker_sort_flights, data)*1000
-    return times
-
-
-
-num_flights = 2000
-generate_random_flights(num_flights)
-flights = load_flights_from_file('flights.txt')
-times = measure_sort_times(flights)
-for sort_algorithm, sort_time in times.items():
-    print(f"{sort_algorithm}: {sort_time} ms")
-
-
-
-
-algorithms = [quicksort_flights,bubble_sort_flights,shaker_sort_flights]
-plot_sort_time_vs_size(algorithms, flights)
