4cc1db2aace5a32ef63ed07a813e66267e4688bd
Nikittua <nisergeev_1@edu.hse.ru>
2023-03-09 23:43:39 +0300

    Переписал функцию шейкер-сортировки
    Переделал функцию сортировкий и добавил документацию к функциям

--- Files/algorithms.py
+++ Files/algorithms.py
@@ -1,40 +1,78 @@
 def compare_flights(flight1, flight2):
     """
-    Compares two flight objects based on a set of rules.
+    .. code-block:: python
 
-    Parameters:
-    flight1 (Flight): A Flight object representing the first flight to compare.
-    flight2 (Flight): A Flight object representing the second flight to compare.
+     1  if flight1.arrival_date != flight2.arrival_date:
+            return (flight1.arrival_date < flight2.arrival_date) * 2 - 1 
 
-    Returns:
-    int: A negative integer if flight1 should come before flight2, a positive integer if flight1 should come after
-    flight2, and zero if the two flights are equal according to the comparison rules.
+     2  elif flight1.arrival_time != flight2.arrival_time:
+            return (flight1.arrival_time < flight2.arrival_time) * 2 - 1
+
+     3  elif flight1.airline != flight2.airline:
+            return (flight1.airline < flight2.airline) * 2 - 1
+            
+     4  else:
+            return (flight2.passengers - flight1.passengers)
+
+
+    **Параметры:**
+    flight1 (Flight)
+    flight2 (Flight)
+
+    **Возвращает:**
+
+    1. Если даты прибытия не совпадают, функция возвращает отрицательное целое число, если рейс1 прибывает раньше рейса2, 
+    или положительное целое число, если рейс1 прибывает позже рейса2. 
+    Выражение (flight1.arrival_date < Flight2.arrival_date) * 2 - 1 используется для преобразования логического результата сравнения в целое число.
+
+    2. Если времена прибытия не равны, возвращается отрицательное целое число, 
+    если рейс1 прибывает раньше рейса2, или положительное целое число, если рейс1 прибывает позже рейса2.
+
+    3. Если авиакомпании не равны, возвращается отрицательное целое число, 
+    если название авиакомпании рейса1 стоит перед названием авиакомпании рейса2 в лексикографическом порядке, 
+    или положительное целое число, если название авиакомпании рейса1 идет после названия авиакомпании рейса2.
+
+    4. Если ни одно из вышеперечисленных условий не выполняется, возвращается разница в количестве пассажиров на двух рейсах, которая будет положительным целым числом, 
+    если у рейса2 больше пассажиров, чем у рейса1, или отрицательным целым числом, 
+    если у рейса2 меньше пассажиров, чем у рейса1.
+
+    
+
     """
     if flight1.arrival_date != flight2.arrival_date:
-        return (flight1.arrival_date < flight2.arrival_date) * 2 - 1
+        return (flight1.arrival_date < flight2.arrival_date) * 2 - 1 
     elif flight1.arrival_time != flight2.arrival_time:
         return (flight1.arrival_time < flight2.arrival_time) * 2 - 1
     elif flight1.airline != flight2.airline:
         return (flight1.airline < flight2.airline) * 2 - 1
     else:
         return (flight2.passengers - flight1.passengers)
-def quicksort_flights(arr):
-    """
-    Sorts an array of Flight objects using the quicksort algorithm.
+def quicksort_flights(flights):
 
-    Parameters:
-    arr (List[Flight]): A list of Flight objects to sort.
-
-    Returns:
-    List[Flight]: The sorted list of Flight objects.
     """
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
+
+    **Quicksort Algorithm:**
+    Алгоритм быстрой сортировки начинается с выбора центрального элемента из списка рейсов,
+    который используется для разделения списка на два меньших списка: один с элементами, меньшими, чем центральный, и другой, с элементами, большими, чем центральный.
+    Затем алгоритм рекурсивно сортирует эти меньшие списки до тех пор, пока не будет отсортирован весь список.
+
+
+    В среднем быстрая сортировка имеет сложность O(n log n), где n — количество элементов в списке.
+    Однако в худшем случае он может иметь временную сложность O (n ^ 2), если центральный элемент выбран неудачно.
+
+    **Параметры:**
+    arr (List[Flight]): список объектов Flight для сортировки.
+
+    **Возвращает:**
+    List[Flight]: отсортированный список объектов Flight.
+    """
+    if len(flights) <= 1:
+        return flights
+    pivot = flights[len(flights) // 2]
     left = []
     right = []
     equal = []
-    for flight in arr:
+    for flight in flights:
         comp = compare_flights(flight, pivot)
         if comp < 0:
             left.append(flight)
@@ -44,14 +82,21 @@ def quicksort_flights(arr):
             equal.append(flight)
     return quicksort_flights(left) + equal + quicksort_flights(right)
 def bubble_sort_flights(flights):
+
     """
-    Sorts an array of Flight objects using the bubble sort algorithm.
+    **Bubble Sort Algorithm:**
+    Алгоритм пузырьковой сортировки проходит по списку полетов, сравнивает соседние элементы и изменяет их местами, если они не удовлетвоярют условию сравнения.
+    Этот процесс повторяется до тех пор, пока список не будет полностью отсортирован.
 
-    Parameters:
-    flights (List[Flight]): A list of Flight objects to sort.
+    
+    Пузырьковая сортировка имеет сложность O(n^2), где n — количество элементов в списке.
+    Обычно он считается из самых медленных алгоритмов сортировки и редко используется на практике для одного большого набора данных.
 
-    Returns:
-    List[Flight]: The sorted list of Flight objects.
+    **Параметры:**
+    Flights (List[Flight]): список объектов Flight для сортировки.
+
+    **Возвращает:**
+    List[Flight]: отсортированный список объектов Flight.
     """
     n = len(flights)
 
@@ -63,43 +108,32 @@ def bubble_sort_flights(flights):
     return flights
 def shaker_sort_flights(flights):
     """
-    Sorts an array of Flight objects using the shaker sort algorithm.
+    **Shaker Sort Algorithm:**
+    Алгоритм шейкерной сортировки — это вариант алгоритма пузырьковой сортировки, в котором список сортируется в обоих направлениях, начиная с обоих концов списка.
+    Он работает, перемещая самый большой элемент вправо и самый маленький элемент влево, пока весь список не будет отсортирован.
 
-    Parameters:
-    flights (List[Flight]): A list of Flight objects to sort.
+    Сортировка шейкером имеет временную сложность O(n^2), где n — количество элементов в списке.
+    Это улучшенная версия пузырьковой сортировки, которая сортирует в обоих направлениях, но имеет ту же временную сложность для наихудшего случая.
 
-    Returns:
-    List[Flight]: The sorted list of Flight objects.
+    **Параметры:**
+    Flights (List[Flight]): список объектов Flight для сортировки.
+
+    **Возвращает:**
+    List[Flight]: отсортированный список объектов Flight.
     """
-    n = len(flights)
     left = 0
-    right = n - 1
-    swapped = True
-
-    while swapped:
-        swapped = False
-
-        # Compare by arrival date (left to right)
+    right = len(flights) - 1
+    while left <= right:
+        # Переместите наибольший элемент вправо
         for i in range(left, right):
             if compare_flights(flights[i], flights[i+1]) > 0:
                 flights[i], flights[i+1] = flights[i+1], flights[i]
-                swapped = True
-
-        if not swapped:
-            break
-
-        swapped = False
         right -= 1
 
-        # Compare by arrival date (right to left)
+        # Переместите наименьший элемент влево
         for i in range(right, left, -1):
             if compare_flights(flights[i-1], flights[i]) > 0:
                 flights[i], flights[i-1] = flights[i-1], flights[i]
-                swapped = True
-
         left += 1
 
     return flights
-
-
-
